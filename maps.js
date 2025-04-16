ed31596b4cf8dc4338310301e28b329925d7caa8
shweta-0249 <yadavshweta699@gmail.com>
2025-04-16 12:13:56 +0530

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,10 @@
+const maps=new Map([
+    ["name","Abhay"],
+    ["age",23],
+    ["mob",9559416764],
+    ["email","abhay36@gmail.com"],
+    ["isMarried",false]
+]);
+console.log(maps);
+
+console.log(maps.get("name"));
